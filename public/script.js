d226dfaf6a2462b294f8eec50fc36f8a87496364
Atsuhito Kita <kitaa@oregonstate.edu>
2020-10-09 03:51:46 -0700

    Fix bug for peer js

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
+  secure: true,
   host: '/',
   port: 443,
   path: '/peerjs'
